5329d95bef7b8c50d4233d671b41d4677a9cedb3
Ereb-999 <Naofumi999@yandex.ru>
2024-02-19 15:06:46 +0400

    chapter zero

---
+++ Application.java
@@ -0,0 +1,13 @@
+package pro.Sky.coursework.curs.the.two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
